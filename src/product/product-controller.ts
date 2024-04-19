bab22f2ffc87c1ee8120a2c6ce56bc17a5eb90c7
ammarahmad <ammar.ahmad6410@gmail.com>
2024-04-19 11:51:37 +0400

    add products.

---
+++ src/product/product-controller.ts
@@ -0,0 +1,183 @@
+import { NextFunction, Response } from 'express';
+import { Request } from 'express-jwt';
+import { v4 as uuidv4 } from 'uuid';
+import { validationResult } from 'express-validator';
+import createHttpError from 'http-errors';
+import { ProductService } from './product-service';
+import { Filter, Product, ProductRequest } from './product-types';
+import { FileStorage } from '../common/types/storage';
+import { UploadedFile } from 'express-fileupload';
+import { AuthRequest } from '../common/types';
+import { Roles } from '../common/constants';
+import mongoose from 'mongoose';
+
+export class ProductController {
+    constructor(
+        private productService: ProductService,
+        private storage: FileStorage,
+    ) {}
+
+    create = async (req: ProductRequest, res: Response, next: NextFunction) => {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return next(createHttpError(400, result.array()[0].msg as string));
+        }
+
+        const image = req.files!.image as UploadedFile;
+        const imageName = uuidv4();
+
+        await this.storage.upload({
+            filename: imageName,
+            fileData: image.data.buffer,
+        });
+
+        const {
+            name,
+            description,
+            priceConfiguration,
+            attributes,
+            tenantId,
+            categoryId,
+            isPublish,
+        } = req.body;
+
+        // todo: fix the types issue
+        const product = {
+            name,
+            description,
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            priceConfiguration: JSON.parse(priceConfiguration),
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            attributes: JSON.parse(attributes),
+            tenantId,
+            categoryId,
+            isPublish,
+            image: imageName,
+        };
+
+        const newProduct = await this.productService.createProduct(
+            product as unknown as Product,
+        );
+
+        res.json({ id: newProduct._id });
+    };
+
+    update = async (req: ProductRequest, res: Response, next: NextFunction) => {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return next(createHttpError(400, result.array()[0].msg as string));
+        }
+
+        const { productId } = req.params;
+
+        const product = await this.productService.getProduct(productId);
+        if (!product) {
+            return next(createHttpError(404, 'Product not found'));
+        }
+
+        if ((req as AuthRequest).auth.role !== Roles.ADMIN) {
+            const tenant = (req as AuthRequest).auth.tenant;
+            if (product.tenantId !== tenant) {
+                return next(
+                    createHttpError(
+                        403,
+                        'You are not allowed to access this product',
+                    ),
+                );
+            }
+        }
+
+        let imageName: string | undefined;
+        let oldImage: string | undefined;
+
+        if (req.files?.image) {
+            oldImage = product.image;
+
+            const image = req.files.image as UploadedFile;
+            imageName = uuidv4();
+
+            await this.storage.upload({
+                filename: imageName,
+                fileData: image.data.buffer,
+            });
+
+            await this.storage.delete(oldImage);
+        }
+
+        const {
+            name,
+            description,
+            priceConfiguration,
+            attributes,
+            tenantId,
+            categoryId,
+            isPublish,
+        } = req.body;
+
+        const productToUpdate = {
+            name,
+            description,
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            priceConfiguration: JSON.parse(priceConfiguration),
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            attributes: JSON.parse(attributes),
+            tenantId,
+            categoryId,
+            isPublish,
+            image: imageName ? imageName : (oldImage as string),
+        };
+
+        await this.productService.updateProduct(productId, productToUpdate);
+
+        res.json({ id: productId });
+    };
+
+    index = async (req: Request, res: Response) => {
+        const { q, tenantId, categoryId, isPublish } = req.query;
+
+        const filters: Filter = {};
+
+        if (isPublish === 'true') {
+            filters.isPublish = true;
+        }
+
+        if (tenantId) filters.tenantId = tenantId as string;
+
+        if (
+            categoryId &&
+            mongoose.Types.ObjectId.isValid(categoryId as string)
+        ) {
+            filters.categoryId = new mongoose.Types.ObjectId(
+                categoryId as string,
+            );
+        }
+
+        // todo: add logging
+        const products = await this.productService.getProducts(
+            q as string,
+            filters,
+            {
+                page: req.query.page ? parseInt(req.query.page as string) : 1,
+                limit: req.query.limit
+                    ? parseInt(req.query.limit as string)
+                    : 10,
+            },
+        );
+
+        const finalProducts = (products.data as Product[]).map(
+            (product: Product) => {
+                return {
+                    ...product,
+                    image: this.storage.getObjectUri(product.image),
+                };
+            },
+        );
+
+        res.json({
+            data: finalProducts,
+            total: products.total,
+            pageSize: products.pageSize,
+            currentPage: products.currentPage,
+        });
+    };
+}
---
+++ src/product/product-types.ts
@@ -0,0 +1,29 @@
+import { Request } from 'express';
+import mongoose from 'mongoose';
+
+export interface Product {
+    _id?: mongoose.Types.ObjectId;
+    name: string;
+    description: string;
+    priceConfiguration: string;
+    attributes: string;
+    tenantId: string;
+    categoryId: string;
+    image: string;
+    isPublish?: string;
+}
+
+export interface Filter {
+    tenantId?: string;
+    categoryId?: mongoose.Types.ObjectId;
+    isPublish?: boolean;
+}
+
+export interface PaginateQuery {
+    page: number;
+    limit: number;
+}
+
+export interface ProductRequest extends Request {
+    body: Product;
+}
